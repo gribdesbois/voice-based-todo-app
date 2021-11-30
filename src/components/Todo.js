b255aa0d64eec7e277693c5249849562365d1f12
gribdesbois <gribdesbois@gmail.com>
2021-11-30 10:58:07 +0100

    initial setup

---
+++ src/components/Todo.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Todo() {
+  return (
+    <div className='todo-main'>
+      <h2 className='header'>Voice-based Todo Application</h2>
+
+      <div className='todo-card'>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Todo
